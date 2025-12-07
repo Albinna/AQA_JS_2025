5f0d6c673888a128e1920e041896408061cdb77e
Albina <a.leonova@gmai.com>
2025-12-07 15:13:45 +0200

    Add nodeBasics folder with index.js and Chalk setup

---
+++ nodeBasics/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+
+console.log(chalk.green('Chalk успішно імпортовано! 💚'));
+console.log(chalk.red('Це червоний текст! ❤️'));
+console.log(chalk.blue.bgYellow('Синій текст на жовтому фоні! 💙'));
