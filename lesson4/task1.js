f3eb87dd6ddfe7b6acc019819938765c0ef4c737
Albina <a.leonova@gmai.com>
2025-12-13 15:42:23 +0200

    Add lesson4.js homework file

---
+++ lesson4/task1.js
@@ -0,0 +1,25 @@
+let string = '34';
+console.log(string);
+
+
+let number = 34
+console.log(number);
+
+
+let user = {
+  name: "Max",
+  age: null
+};
+
+console.log(user.age);
+
+
+let testString = "Hello world"
+console.log(testString[12]);
+console.log(testString.indexOf('u'))
+
+
+let undefined;
+console.log (undefined);
+
+
---
+++ lesson4/task2.js
@@ -0,0 +1,9 @@
+let name1 = 'Max';
+let name2 = 'Alex';
+
+let concatString = 'Dear, ' + name1 + ' and ' + name2 + ', congratulations on your work anniversary with the company!';
+let concatString2 = `Dear, ${name1} and ${name2}, congratulations on your work anniversary with the company!`;
+
+console.log(concatString);
+console.log(concatString2);
+
