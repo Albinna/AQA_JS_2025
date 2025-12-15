fd78c73989801f0455bc61532d0a4a7e8d2864ed
Albina <a.leonova@gmai.com>
2025-12-15 11:32:23 +0200

    Adding the solution for task4 to the lesson4 folder

---
+++ lesson4/task4.js
@@ -0,0 +1,20 @@
+// task4.1
+let radiusCircle = 5; 
+let areaCircle = Math.PI * Math.pow(radiusCircle, 2);
+console.log(areaCircle.toFixed(2));
+
+// task4.2
+let length = 10;
+let width = 4;
+let areaRectangle = length * width;
+console.log(areaRectangle.toFixed(2));
+
+//task4.3
+let radius2 = 4.5; 
+let height = 10.2; 
+const pi2 = Math.PI; 
+let volume = pi2 * Math.pow(radius2, 2) * height; volume = volume.toFixed(2); 
+
+console.log("Radius:", radius2.toFixed(2)); 
+console.log("Height:", height.toFixed(2)); 
+console.log("Cylinder volume:", volume);
---
+++ lesson4/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+// task4.1
+let radiusCircle = 5; 
+let areaCircle = Math.PI * Math.pow(radiusCircle, 2);
+console.log(areaCircle.toFixed(2));
+
+// task4.2
+let length = 10;
+let width = 4;
+let areaRectangle = length * width;
+console.log(areaRectangle.toFixed(2));
