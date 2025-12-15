7a277689d3e2e947e1aa0f3fe000a93a8ec069a8
Albina <a.leonova@gmai.com>
2025-12-15 11:26:51 +0200

    Stop tracking tempCodeRunnerFile in lesson4

--- lesson4/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-let radius2 = 4.5;
-let height = 10.2;
-const pi2 = Math.PI;
-let volume = pi2 * Math.pow(radius2, 2) * height;
-volume = volume.toFixed(2);
-
-console.log("Radius:", radius2.toFixed(2));
-console.log("Height:", height.toFixed(2));
-console.log("Cylinder volume:", volume);
